02e55f6ad6c26178e2ed869b69e972198cd9fa6b
Armaan Chahal <armaan.c2009@gmail.com>
2025-07-21 17:18:39 -0500

    add base raylib render code

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,8 +1,34 @@
-#include <iostream>
 #include "main.h"
+
+#include "raylib.h"
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    const int scrWidth = 800;
+    const int scrHeight = 450;
+
+    InitWindow(scrWidth, scrHeight, "Langton's Ant");
+
+    int framesCounter = 0;
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        framesCounter++;
+
+        if (IsKeyPressed(KEY_ESCAPE))
+        {
+            break;
+        }
+
+        ClearBackground(WHITE);
+
+        BeginDrawing();
+
+        DrawRectangle(0, 0, scrWidth - 10, scrHeight - 7, RED);
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
